59ee5513e39ae0e3f0c3435a6b7a012be9947da0
Scott Ware <srware@ubuntu.(none)>
2015-05-26 15:32:13 +0100

    Add variables for easy configiration and customisation.

--- src/upnp-service.js
+++ src/upnp-service.js
@@ -3,7 +3,14 @@ var http = require("http");
 var server = http.createServer();
 var PORT = 8080;
 
-// start server on port 8080.
+var name="IOT-DEVICE";
+var model="IoT Device";
+var modelUrl="";
+var version="1.00";
+var serial="12345678";
+var address="";
+
+// Start server on port 8080.
 server.listen(PORT);
 
 // Create a UPnP Peer. 
@@ -12,7 +19,7 @@ var peer = upnp.createPeer({
     server: server
 }).on("ready",function(peer){
     console.log("ready");
-    // advertise device after peer is ready
+    // Advertise device after peer is ready
     device.advertise();
 }).on("close",function(peer){
     console.log("closed");
@@ -21,19 +28,17 @@ var peer = upnp.createPeer({
 // Create a Basic device.
 var device = peer.createDevice({
     autoAdvertise: true,
-    uuid: "",
     productName: "IoT Reference Platform",
-    productVersion: "1.04",
     domain: "schemas-upnp-org",
     type: "Basic",
     version: "1",
-    friendlyName: "IOTRP-DEVICE",
+    friendlyName: name,
     manufacturer: "Intel",
     manufacturerURL: "http://www.intel.com/content/www/us/en/homepage.html",
-    modelName: "Edison",
+    modelName: model,
     modelDescription: "Intel IoT Device",
-    modelNumber: "Edison",
-    modelURL: "",
-    serialNumber: "",
-    UPC: ""
+    modelNumber: version,
+    modelURL: modelUrl,
+    serialNumber: serial,
+    presentationURL: address
 });
